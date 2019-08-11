c90c39dcaf2672561bf2a5eb8fb1eacdb1c0a1ed
Davis Hochstatter <davishochs@gmail.com>
2019-08-10 22:08:46 -0500

    it works mostly, messy as fuck but I tried

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,114 @@
-const possibleAnswers = ['computer'];
-const mysteryWord = [];
+const possibleAnswers = ['computer', 'toaster', 'apple', 'jar', 'monitor', 'congress', 'idiot', 'javascript'];
 let guessDisplay = document.querySelector('#word-chamber');
-let guessCounter = 0;
+let yourGuess = document.querySelector('#letters-guessed');
+let guessesRemain = document.querySelector('#guess-counter');
+let guessCounter = 10;
 let winCounter = 0;
+let lossCounter = 0;
 
-//Random number generator used for selecting a random word from the array of choices
-function getRandomNum(n) {
+function getRandomNumber(n) {
     return Math.floor(Math.random() * n);
-};
+}
 
-//R
-let randomIndex = getRandomNum(possibleAnswers.length);
-const randomWord = possibleAnswers[randomIndex];
+function getRandomWord(number) {
+    let randomIndex = getRandomNumber(number);
+    let randomWord = possibleAnswers[randomIndex];
+    return randomWord;
+}
 
-function loadRandomWordAndDisplay() {
-
+function loadGuessDisplay(randomWord) {
+    const mysteryWord = [];
+    let guessDisplay = document.querySelector('#word-chamber');
+    
     for (let i = 0; i < randomWord.length; i++) {
-        let guessDisplay = document.querySelector('#word-chamber');
         mysteryWord.push('_ ');
         mysteryWord.toString();
         guessDisplay.textContent = mysteryWord;
-        
-        
     }
     return mysteryWord;
-};
+}
 
+function checkWinCondition(arr1, arr2) {
+    let check1 = arr1.join();
+    let check2 = arr2.join();
+    
+    if (check1 === check2) {
+        return true;
+    }
+}
 
-// Gives me an array of underscores the length of the word assigned to unknownWord
-const unknownWord = loadRandomWordAndDisplay();
-console.log(unknownWord);
+function reset() {
 
-
+    guessesRemain.innerText = guessCounter;
+    guessDisplay.innerHTML = '';
+    yourGuess.innerHTML = '';
+   
+    
+    runGame();
+}
 
 function runGame() {
-    let guessCounter = unknownWord.length + 5;
-    let guessesRemain = document.querySelector('#guess-number');
-    guessesRemain.textContent += guessCounter;
+    //Sets up our variables for the games uses
+    let randomWord = getRandomWord(possibleAnswers.length);
+    let unknownWord = loadGuessDisplay(randomWord);
+    const split = randomWord.split('');
+    const answerContainer = Array(randomWord.length);
     
-
     
-
+    let wins = document.querySelector('#wins');
+    
+    
+    
+    
+    
     document.onkeyup = function(e) {
         const userSelection = e.key.toLowerCase();
-        const split = randomWord.split('');
-        let yourGuess = document.querySelector('#your-guess');
         yourGuess.innerText += userSelection;
         
+        guessesRemain = document.querySelector('#guess-counter');
+        guessesRemain.textContent = guessCounter;
+        guessCounter--;
+        
+
+        
+
         for (let i = 0; i < split.length; i++) {
-            if (split.includes(userSelection)) {
-                
-                if (split[i] == userSelection) {
+            
+                if (split[i] == userSelection && e.keyCode >= 65 && e.keyCode <= 90) {
                     let guessDisplay = document.querySelector('#word-chamber');
                     unknownWord[i] = userSelection;
                     guessDisplay.innerText = unknownWord;
-                    
-
+                    answerContainer[i] = userSelection;    
                 }
+            }
+            let hasWon = checkWinCondition(split, answerContainer);
+            if (hasWon == true) {
+                alert('You win!'); 
+                let wins = document.querySelector('#win-display'); 
+                winCounter++;
+                wins.textContent  = winCounter;
+                guessCounter = 10;
                 
             }
-        }
+            else if (guessCounter < 0) {
+                alert('You lose!');
+                let loss = document.querySelector('#loss-display');
+                lossCounter++;
+                loss.textContent = lossCounter;
+                guessCounter = 10;
+                
+            }
+                 
     }
-};
+    
+    
+}
 runGame();
 
 
- 
+
+
+
+
+
+
